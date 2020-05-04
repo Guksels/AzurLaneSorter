1f801b84e610a639f32ee3e125881fa2720c5b86
Guksels <yogukssis@gmail.com>
2020-05-05 02:53:52 +0300

    DataTest

--- src/js/data.js
+++ src/js/data.js
@@ -20,6 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-// const imageRoot = 'https://i.imgur.com/';
-//const imageRoot = 'guksels.github.io/AzurLaneSorter/src/assets/ShipImages/';
 const imageRoot = '/AzurLaneSorter/src/assets/ShipImages/';
